1d20fdda7099d565e13cbc2d1eb0e4782b0ef17b
Ye-Florez <yelitzsarayfo@ufps.edu.co>
2026-04-18 11:05:08 -0500

    Agregue la opcion, para volver a hacer una operacion

--- CalculadoraArqui/src/calculadoraarqui/Main.java
+++ CalculadoraArqui/src/calculadoraarqui/Main.java
@@ -14,24 +14,33 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String continuar;
+        do{
+            System.out.println("=== Calculadora Multi-Base ===");
+            System.out.println("Sistema: 10=Decimal  2=Binario  16=Hexadecimal  8=Octal");
+            System.out.print("Elige sistema: ");
+            int sistema = sc.nextInt();
 
-        System.out.println("=== Calculadora Multi-Base ===");
-        System.out.println("Sistema: 10=Decimal  2=Binario  16=Hexadecimal  8=Octal");
-        System.out.print("Elige sistema: ");
-        int sistema = sc.nextInt();
+            System.out.print("Operando 1: ");
+            String op1 = sc.next();
 
-        System.out.print("Operando 1: ");
-        String op1 = sc.next();
+            System.out.print("Operación (+, -, *, /): ");
+            char operacion = sc.next().charAt(0);
 
-        System.out.print("Operación (+, -, *, /): ");
-        char operacion = sc.next().charAt(0);
+            System.out.print("Operando 2: ");
+            String op2 = sc.next();
 
-        System.out.print("Operando 2: ");
-        String op2 = sc.next();
-
-        CalculadoraArqui calc = new CalculadoraArqui(op1, op2, sistema, operacion);
-        String resultado = calc.operar(operacion);
-
-        System.out.println("Resultado: " + resultado);
+            CalculadoraArqui calc = new CalculadoraArqui(op1, op2, sistema, operacion);
+            String resultado = calc.operar(operacion);
+            System.out.println("Resultado: " + resultado); 
+            
+            System.out.print("\n¿Desea realizar otra operación? (SI/NO): ");
+            continuar = sc.next().toLowerCase();
+            
+        }while(continuar.equals("si") || continuar.equals("s"));
+        
+        System.out.println("¡Hasta luego!");
+        sc.close();
     }
 }
+
